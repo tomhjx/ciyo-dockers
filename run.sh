3c2d60f4ddf89cb308fa2aefccfd1af279d4a0b3
疯狂的茶几 <tom_hejiaxi@163.com>
2018-03-26 20:42:03 +0800

    优化脚本

--- run.sh
+++ run.sh
@@ -12,6 +12,10 @@ case $1 in
 
     'push-git-all')
     read -p  "输入该次git提交的备注说明：" msg
+
+    git config --global user.name "疯狂的茶几"
+    git config --global user.email tom_hejiaxi@163.com
+    
     git add -A . && git commit -m "$msg" && git push
     ;;
 esac
